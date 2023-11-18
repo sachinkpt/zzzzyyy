8e61e7f2862cac3c37b43b798127d76f13094713
sachinkpt <145140896+sachinkpt@users.noreply.github.com>
2023-11-18 22:53:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -290,7 +290,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ᴊᴏɪɴ ᴛᴏ ᴄʜᴀɴɴᴇʟ', url=(MAIN_CHANNEL)) ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Main Channel', url=(MAIN_CHANNEL)) ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
